ae8a0840cd36062283240a2dfb68c1b0ef8b8963
ArielFariasEP <ariel@ephealth.com.br>
2024-03-06 21:44:39 -0300

    feat: corrige inicializacao do banco

--- datasource/db.go
+++ datasource/db.go
@@ -13,7 +13,7 @@ type Database struct {
 func Connect() Database {
 	var DB Database
 
-	db, err := gorm.Open(postgres.Open("postgres://postgres@localhost:5432/postgres"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("host=localhost password=postgres dbname=postgres port=5432 user=postgres sslmode=disable"), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
